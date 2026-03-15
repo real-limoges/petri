b0f947f316b87b4551e1751e22245030c13b88a3
Real Limoges <b.real.limoges@gmail.com>
2026-03-15 08:54:48 -0700

    add wasm for fugue

--- build_all.sh
+++ build_all.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 CC="${CC:-$(brew --prefix llvm)/bin/clang}"
-FLAGS="--target=wasm32-unknown-unknown -O2 -flto -nostdlib -Wl,--no-entry -Wl,--export-dynamic"
 
 mkdir -p wasm
 
-$CC $FLAGS -Wl,--initial-memory=33554432  -o wasm/boids.wasm       src/boids.c
-$CC $FLAGS -Wl,--initial-memory=16777216  -o wasm/langton.wasm     src/langton.c
-$CC $FLAGS -Wl,--initial-memory=16777216  -o wasm/oscillators.wasm src/oscillators.c
-$CC $FLAGS -Wl,--initial-memory=67108864  -o wasm/physarum.wasm    src/physarum.c
+$CC --target=wasm32-unknown-unknown -O2 -flto -nostdlib \
+    -Wl,--no-entry -Wl,--export-dynamic \
+    -Wl,--initial-memory=33554432 \
+    -o wasm/boids.wasm src/boids.c
+
+$CC --target=wasm32-unknown-unknown -O2 -flto -nostdlib \
+    -Wl,--no-entry -Wl,--export-dynamic \
+    -Wl,--initial-memory=16777216 \
+    -o wasm/langton.wasm src/langton.c
+
+$CC --target=wasm32-unknown-unknown -O2 -flto -nostdlib \
+    -Wl,--no-entry -Wl,--export-dynamic \
+    -Wl,--initial-memory=16777216 \
+    -o wasm/oscillators.wasm src/oscillators.c
+
+$CC --target=wasm32-unknown-unknown -O2 -flto -nostdlib \
+    -Wl,--no-entry -Wl,--export-dynamic \
+    -Wl,--initial-memory=67108864 \
+    -o wasm/physarum.wasm src/physarum.c
 
 echo "Built all WASM modules."
